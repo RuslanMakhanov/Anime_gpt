35bbba544b3ec140c70b85a7d63770642ac121d4
PilotOfAsuka <150352161+PilotOfAsuka@users.noreply.github.com>
2024-05-02 14:00:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def main():
     # Создание задачи для отправки периодических сообщений
     asyncio.ensure_future(send_message_at(hour=9, minutes=00, text=send_anime_girl("Спокойной ночи Руслану, пожелание тебя, для Руслана", "admin",), chat_id=ram_rem_chat))
     asyncio.ensure_future(send_message_at(hour=20, minutes=00, text=send_anime_girl("Доброе утро Руслану, пожелание для Руслана","admin",), chat_id=ram_rem_chat))
-    asyncio.ensure_future(send_message_at(hour=13, minutes=00, text=send_anime_girl(task=f"Илья(PilotAski) не курит уже {days_since_last(smoke_free_users.get('5848061277'))}, пожелание ему!", user_name="admin"), chat_id=ram_rem_chat))
+    asyncio.ensure_future(send_message_at(hour=19, minutes=00, text=send_anime_girl(task=f"Илья(PilotAski) не курит уже {days_since_last(smoke_free_users.get('5848061277'))}, пожелание ему!", user_name="admin"), chat_id=ram_rem_chat))
 
     # Регистрация функции on_startup
     misc.dp.startup.register(on_startup)
