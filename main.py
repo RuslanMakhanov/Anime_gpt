e7b52bf2b2a7854aec53c8912bd5e8064bc36721
PilotOfAsuka <150352161+PilotOfAsuka@users.noreply.github.com>
2024-05-08 13:38:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -13,12 +13,25 @@ async def on_startup():
 
 
 async def main():
-    while True:
-        # Создание задачи для отправки периодических сообщений
-        asyncio.ensure_future(send_message_at(hour=9, minutes=00, text=send_anime_girl("Спокойной ночи Руслану, пожелание тебя, для Руслана", "admin",), chat_id=ram_rem_chat))
-        asyncio.ensure_future(send_message_at(hour=20, minutes=00, text=send_anime_girl("Доброе утро Руслану, пожелание для Руслана","admin",), chat_id=ram_rem_chat))
-        asyncio.ensure_future(send_message_at(hour=19, minutes=00, text=send_anime_girl(task=f"Илья(PilotAski) не курит уже {days_since_last(smoke_free_users.get('5848061277'))}, пожелание ему!", user_name="admin"), chat_id=ram_rem_chat))
+    global message_tasks
 
+    while True:
+        # Отменяем предыдущие задачи
+        for task in message_tasks:
+            print(f"{task}: Удалена!")
+            task.cancel()
+
+
+        message_tasks = [asyncio.create_task(send_message_at(hour=9, minutes=00, text=send_anime_girl(
+            "Спокойной ночи Руслану(@RuslanNnNn1337), пожелание от тебя, для Руслана", "admin", ), chat_id=ram_rem_chat)),
+                         asyncio.create_task(send_message_at(hour=20, minutes=00, text=send_anime_girl(
+                             "Доброе утро Руслану(@RuslanNnNn1337), пожелание от тебя для Руслана", "admin", ), chat_id=ram_rem_chat)),
+                         asyncio.create_task(send_message_at(hour=19, minutes=00, text=send_anime_girl(
+                             task=f"Илья(@PilotAski) не курит уже {days_since_last(smoke_free_users.get('5848061277'))}, пожелание ему! и сделай сравнение как это долго.",
+                             user_name="admin"), chat_id=ram_rem_chat))]
+
+        print(f"{get_time_text(date=True)}: Созданы новые задачи")
+        # Ждем один день перед повторным обновлением задач
         await asyncio.sleep(24 * 60 * 60)
 
 async def startup_task():
@@ -31,6 +44,7 @@ async def startup_task():
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
+    message_tasks = []
 
     try:
         loop.run_until_complete(asyncio.gather(main(), startup_task()))
